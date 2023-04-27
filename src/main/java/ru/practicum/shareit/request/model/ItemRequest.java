6b67938b7bb59cee85457a79daa406bca314e4ca
Vitaliy <vit.ivoylov@yandex.ru>
2023-04-27 22:59:40 +0500

    feat:add class ItenRequest

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.request.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
+@AllArgsConstructor
+@Component
 public class ItemRequest {
+    private Long id;
+    private String description;
+    private String requestor;
+    private LocalDateTime created;
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,12 +2,14 @@ package ru.practicum.shareit.user.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 /**
  * TODO Sprint add-controllers.
  */
 @Data
 @AllArgsConstructor
+@Component
 public class User {
     private Long id;
     private String name;
