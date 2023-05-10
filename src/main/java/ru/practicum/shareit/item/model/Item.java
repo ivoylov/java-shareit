307bddcd33cb57081762451ae863d3afd59e0a1a
Vitaliy <vit.ivoylov@yandex.ru>
2023-05-10 21:03:57 +0500

    refactor:delete validation from Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,22 +3,14 @@ package ru.practicum.shareit.item.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 @Builder
-@RequiredArgsConstructor
 public class Item {
     private Long id;
-    @NotEmpty
     private String name;
-    @NotEmpty
     private String description;
-    @NotNull
     private Boolean available;
     private Long ownerId;
     private Long requestId;
