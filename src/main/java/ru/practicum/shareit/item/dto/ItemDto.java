c01c576695fff4450da3bf5dbc77cc036a455a8c
Vitaliy <vit.ivoylov@yandex.ru>
2023-05-12 22:56:22 +0500

    refactor:delete static in mapper class

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -12,7 +13,7 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class ItemDto {
     private Long id;
-    @NotEmpty
+    @NotBlank
     private String name;
     @NotEmpty
     private String description;
--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoMapper.java
@@ -6,7 +6,7 @@ import ru.practicum.shareit.item.model.Item;
 @UtilityClass
 public class ItemDtoMapper {
 
-    public static ItemDto toItemDto(Item item) {
+    public ItemDto toItemDto(Item item) {
         return ItemDto.builder()
                 .id(item.getId())
                 .name(item.getName())
@@ -16,7 +16,7 @@ public class ItemDtoMapper {
                 .build();
     }
 
-    public static Item toItem(ItemDto itemDto) {
+    public Item toItem(ItemDto itemDto) {
         return Item.builder()
                 .id(itemDto.getId())
                 .name(itemDto.getName())
