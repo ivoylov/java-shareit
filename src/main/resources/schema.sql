9b51c829a90129ec832d4b286881d951e4cd631d
Vitaliy <vit.ivoylov@yandex.ru>
2023-05-21 12:33:00 +0500

    feat:add tables bookings, requests, comments to db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,3 +11,26 @@ CREATE TABLE IF NOT EXISTS items (
     available boolean not null,
     owner_id bigint not null references shareit_user(id)
 );
+
+CREATE TABLE IF NOT EXISTS bookings (
+    id bigint not null generated always as identity primary key ,
+    start_date date not null,
+    end_date date not null,
+    item_id bigint not null ,
+    booker_id bigint not null,
+    status int
+);
+
+CREATE TABLE IF NOT EXISTS bookings (
+    id bigint not null generated always as identity primary key ,
+    description varchar(255),
+    requestor_id bigint
+);
+
+CREATE TABLE IF NOT EXISTS bookings (
+    id bigint not null generated always as identity primary key ,
+    text varchar(255),
+    item_id bigint,
+    author_id bigint
+);
+
