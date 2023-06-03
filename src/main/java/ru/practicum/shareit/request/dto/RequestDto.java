eec1438c3050aa9adcb28e668710ba78a4baad80
Vitaliy <vit.ivoylov@yandex.ru>
2023-06-03 15:19:05 +0500

    feat:add annotation to RequestDto

--- src/main/java/ru/practicum/shareit/request/dto/RequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/RequestDto.java
@@ -3,15 +3,21 @@ package ru.practicum.shareit.request.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import ru.practicum.shareit.Create;
+import ru.practicum.shareit.Update;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class RequestDto {
-    private Long id;
+    @NotNull (groups = {Update.class, Create.class}, message = "описание не может быть пустым")
+    @NotBlank (groups = {Update.class, Create.class}, message = "должно быть описание")
     private String description;
+    @NotNull (groups = {Create.class, Update.class}, message = "Необходим id пользователя")
     private String requestor;
     private LocalDateTime created;
 }
