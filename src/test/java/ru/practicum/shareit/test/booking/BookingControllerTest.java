961421ff5844aa3c120058f9a549b0366bbb24e8
Vitaliy <vit.ivoylov@yandex.ru>
2023-06-10 21:42:21 +0500

    feat:complete bookingController tests

--- src/test/java/ru/practicum/shareit/test/booking/BookingControllerTest.java
+++ src/test/java/ru/practicum/shareit/test/booking/BookingControllerTest.java
@@ -1,35 +1,29 @@
 package ru.practicum.shareit.test.booking;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.practicum.shareit.booking.controller.BookingController;
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.dto.TestBookingDto;
 import ru.practicum.shareit.booking.service.BookingPageableService;
-import ru.practicum.shareit.booking.service.BookingService;
 
-import java.nio.charset.StandardCharsets;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -40,69 +34,56 @@ class BookingControllerTest {
     private BookingController bookingController;
     @Mock
     private BookingPageableService bookingService;
-    private MockMvc mvc;
     private BookingDto bookingDto;
-    private Long userId;
-    private ObjectMapper mapper;
+    private List<BookingDto> bookingDtoList;
+    private Long id;
 
     @BeforeEach
     void setUp() {
 
-        mapper = JsonMapper.builder()
-                .findAndAddModules()
-                .build();
-
         LocalDateTime start = LocalDateTime.now(Clock.systemDefaultZone()).plusHours(1);
         LocalDateTime end = LocalDateTime.now(Clock.systemDefaultZone()).plusHours(2);
-
-        userId = 1L;
-
-        mvc = MockMvcBuilders
-                .standaloneSetup(bookingController)
-                .build();
+        id = 1L;
 
         bookingDto = BookingDto.builder()
-                .itemId(1L)
+                .itemId(id)
                 .start(start)
                 .end(end)
-                .bookerId(userId)
+                .bookerId(id)
                 .build();
+        bookingDtoList = new ArrayList<>();
+        bookingDtoList.add(bookingDto);
 
     }
 
     @Test
-    void create() throws Exception {
-
-        when(bookingService.create(any()))
-                .thenReturn(bookingDto);
-
-        mvc.perform(MockMvcRequestBuilders.post("/bookings")
-                        .content(mapper.writeValueAsString(bookingDto))
-                        .characterEncoding(StandardCharsets.UTF_8)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("X-Sharer-User-Id", userId)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.itemId", is(bookingDto.getId()), Long.class))
-                .andExpect(jsonPath("$.bookerId", is(bookingDto.getBookerId())))
-                .andExpect(jsonPath("$.start", is(bookingDto.getStart())))
-                .andExpect(jsonPath("$.end", is(bookingDto.getEnd())));
-
+    void create() {
+        when(bookingService.create(any())).thenReturn(bookingDto);
+        assertEquals(bookingController.create(bookingDto, id), bookingDto);
     }
 
     @Test
     void approved() {
+        when(bookingService.update(any())).thenReturn(bookingDto);
+        assertEquals(bookingController.approved(id,id,true), bookingDto);
     }
 
     @Test
     void get() {
+        when(bookingService.get(id,id)).thenReturn(bookingDto);
+        assertEquals(bookingController.get(id,id), bookingDto);
     }
 
     @Test
     void getAllForBooker() {
+        when(bookingService.getAllForBooker(id, "FUTURE", 1, 1)).thenReturn(bookingDtoList);
+        assertEquals(bookingController.getAllForBooker(id,"FUTURE",1,1), bookingDtoList);
     }
 
     @Test
-    void testGet() {
+    void getAllForOwner() {
+        when(bookingService.getAllForOwner(id,"FUTURE", 1, 1)).thenReturn(bookingDtoList);
+        assertEquals(bookingController.get(id,"FUTURE",1,1), bookingDtoList);
     }
+
 }
