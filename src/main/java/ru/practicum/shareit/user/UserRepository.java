7a4878fb4d461a4cb7a12dc09ff96c46c1641cdf
ivoylov <vit.ivoylov@yandex.ru>
2023-07-05 08:57:38 +0500

    feat:connect to db

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -3,10 +3,12 @@ package ru.practicum.shareit.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.model.User;
 
 import javax.transaction.Transactional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Transactional
