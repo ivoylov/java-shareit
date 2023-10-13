6e16636e84394ad3c1419e37eaed72ffc4333fd0
ivoylov <vit.ivoylov@yandex.ru>
2023-10-13 21:55:05 +0500

    style:change same path in requestControllers

--- gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
@@ -18,28 +18,30 @@ import javax.validation.constraints.Min;
 public class RequestController {
 
     private final RequestClient requestClient;
+    private final String userIdHeader = "X-Sharer-User-Id";
+    private final int minUserId = 1;
 
     @PostMapping
     public ResponseEntity<Object> created(@Validated(Create.class) @RequestBody RequestDtoIn requestDtoIn,
-                                   @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
+                                   @RequestHeader(userIdHeader) @Min(minUserId) Long userId) {
         return requestClient.createRequest(requestDtoIn, userId);
     }
 
     @GetMapping
-    public ResponseEntity<Object> getAllUserRequests(@RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
+    public ResponseEntity<Object> getAllUserRequests(@RequestHeader(userIdHeader) @Min(minUserId) Long userId) {
         return requestClient.getAllItemRequest(userId);
     }
 
     @GetMapping("/all")
     public ResponseEntity<Object> getAllByAnotherUsers(@RequestParam(defaultValue = "0") @Min(0) Integer from,
                             @RequestParam(defaultValue = "10") @Min(1) @Max(100) Integer size,
-                            @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
+                            @RequestHeader(userIdHeader) @Min(minUserId) Long userId) {
         return requestClient.getAllRequests(userId, from, size);
     }
 
     @GetMapping("/{requestId}")
     public ResponseEntity<Object> get(@PathVariable Long requestId,
-                      @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
+                      @RequestHeader(userIdHeader) @Min(minUserId) Long userId) {
         return requestClient.getItemRequestById(userId, requestId);
     }
 
--- server/src/main/java/ru/practicum/shareit/request/RequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/RequestController.java
@@ -22,29 +22,31 @@ import java.util.List;
 public class RequestController {
 
     private final RequestService requestService;
+    private final String userIdHeader = "X-Sharer-User-Id";
+    private final int minUserId = 1;
 
     @PostMapping
     RequestDtoOut created(@Validated(Create.class) @RequestBody RequestDtoIn requestDtoIn,
-                          @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
+                          @RequestHeader(userIdHeader) @Min(minUserId) Long userId) {
         Request request = RequestMapper.toRequest(requestDtoIn);
         return RequestMapper.toRequestDtoOut(requestService.create(request, userId));
     }
 
     @GetMapping
-    List<RequestDtoOut> getAllUserRequests(@RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
+    List<RequestDtoOut> getAllUserRequests(@RequestHeader(userIdHeader) @Min(minUserId) Long userId) {
         return RequestMapper.toListRequestDtoOut(requestService.getUserRequests(userId));
     }
 
     @GetMapping("/all")
     List<RequestDtoOut> getAllByAnotherUsers(@RequestParam(defaultValue = "0") @Min(0) Integer from,
                             @RequestParam(defaultValue = "10") @Min(1) @Max(100) Integer size,
-                            @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
+                            @RequestHeader(userIdHeader) @Min(minUserId) Long userId) {
         return RequestMapper.toListRequestDtoOut(requestService.getWithPagination(from, size, userId));
     }
 
     @GetMapping("/{requestId}")
     RequestDtoOut get(@PathVariable Long requestId,
-                      @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
+                      @RequestHeader(userIdHeader) @Min(minUserId) Long userId) {
         return RequestMapper.toRequestDtoOut(requestService.get(requestId, userId));
     }
 
